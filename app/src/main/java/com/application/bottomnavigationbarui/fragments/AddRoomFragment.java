946de69efbc8ce800ff12aa7583f38d6ebfe39e5
DevFrogora <rr4428310@gmail.com>
2026-06-04 09:09:41 +0530

    add room

--- app/src/main/java/com/application/bottomnavigationbarui/fragments/AddRoomFragment.java
+++ app/src/main/java/com/application/bottomnavigationbarui/fragments/AddRoomFragment.java
@@ -2,6 +2,8 @@ package com.application.bottomnavigationbarui.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,17 +13,21 @@ import android.view.ViewGroup;
 import com.application.bottomnavigationbarui.R;
 import com.application.bottomnavigationbarui.databinding.FragmentAddRoomBinding;
 import com.application.bottomnavigationbarui.databinding.FragmentBillsBinding;
+import com.application.bottomnavigationbarui.utils.ErrorUtils;
+import com.application.bottomnavigationbarui.utils.UiHelper;
+import com.github.devfrogora.service.RoomMeterService;
+import com.github.devfrogora.service.impl.RoomMeterServiceImpl;
 
 
 public class AddRoomFragment extends Fragment {
 
     private FragmentAddRoomBinding binding;
+    private UiHelper ui;
 
 
     public AddRoomFragment() {
         // Required empty public constructor
     }
-
 
 
     @Override
@@ -35,5 +41,40 @@ public class AddRoomFragment extends Fragment {
         // Inflate the layout for this fragment
         binding = FragmentAddRoomBinding.inflate(inflater, container, false);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ui = new UiHelper(this.getContext());
+        binding.layoutAddroom.btnAddRoom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String roomNumber = binding.layoutAddroom.etRoomNumber.getText().toString();
+                String roomType = binding.layoutAddroom.etRoomType.getText().toString();
+                String meterSerial = binding.layoutAddroom.etMeterSerial.getText().toString();
+                String initialReading = binding.layoutAddroom.etInitialReading.getText().toString();
+
+                // Perform validation and save logic here
+
+                RoomMeterService roomMeterService = new RoomMeterServiceImpl();
+                try {
+                  roomMeterService.addRoomWithMeter(roomNumber, roomType, meterSerial, Double.parseDouble(initialReading));
+                } catch(Exception e){
+                    ErrorUtils.handleDatabaseException("Error initializing database", e, ui);
+                }
+            }
+        });
+
+        binding.layoutAddroom.btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.layoutAddroom.etRoomNumber.setText("");
+                binding.layoutAddroom.etRoomType.setText("");
+                binding.layoutAddroom.etMeterSerial.setText("");
+                binding.layoutAddroom.etInitialReading.setText("");
+            }
+        });
     }
 }
